2f1c8c019baca3914ef277af1350e5831239fba3
Rowalewa <lesliewanjala06@gmail.com>
2024-03-12 13:43:42 +0300

    KotAlpha

---
+++ src/KotlinOne.kt
@@ -0,0 +1,49 @@
+fun main(args: Array<String>) {
+    // integer
+    // string
+    // double
+    // Char
+    // Boolean
+    var a: Int
+    var b: String
+    var c: Double
+    var d: Char
+    var e: Boolean
+
+    a = 777
+    b = "This is my First Kotlin"
+    c = 12.65
+    d = 's'
+    e = true
+
+    var z = 13
+    var division = a/c
+    var difference = a-c
+
+    println(a+c)
+    println(difference)
+    println(a*c)
+    println(division)
+
+    val y: Int
+    z = 24
+    val x: String
+    x = "Hello"
+
+    var age: Int
+    age = 18
+    var height: Int
+    height = 45
+    println("Your age is $age and height is $height")
+    println("Your age is " +age+ " and height is " +height)
+
+    var t: Int
+    t = 33
+    var u: Double
+    u = 0.77
+    var v: Int
+    v = 47
+    var score = t+u+v
+    println("Your score is $score out of 100%")
+    println("$t out of $v counties have had rapid developments for the last one decade")
+}
